ab7d96f945b4342dd60b1af2fe0b353ea67211f1
paragaggarwal <paragaggarwal04@gmail.com>
2023-09-06 12:22:56 +0530

    Taxi APP

---
+++ components/Map/MapBoxRoute.tsx
@@ -0,0 +1,28 @@
+import { type } from 'os';
+import React from 'react'
+import Map, { AttributionControl, GeolocateControl, Layer, Marker, NavigationControl,
+Popup, Source} from "react-map-gl";
+function MapBoxRoute(props:any) {
+
+    return (
+        <Source
+  type="geojson"
+  data={{
+    type: 'Feature',
+    geometry: {
+      type: 'LineString',
+      coordinates: props.coordinates,
+    },
+    properties: {}, // Add an empty properties object
+  }}
+>
+  <Layer
+    type="line"
+    layout={{ 'line-join': 'round', 'line-cap': 'square' }}
+    paint={{ 'line-color': '#0462d4', 'line-width': 4 }}
+  />
+</Source>
+      )
+    }
+    
+    export default MapBoxRoute
